132289472d53a3152d7d2c871ff1763354e937a8
Lex Myers <lex.myers@gmail.com>
2017-11-29 13:20:13 -0800

    messing around

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  number = 10;
+  name = 'Bill Gates';
+  user = {
+    firstName: 'Darth',
+    lastName: 'Vader'
+  }
+  loggedIn = true;
+  stuff = ['these', 'are', 'some', 'words'];
 }
