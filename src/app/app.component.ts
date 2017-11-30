ae328479291d82f1dcbf659244409eef949d6638
Lex Myers <lex.myers@gmail.com>
2017-11-30 15:15:23 -0800

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user = { email: '' }
+  title = 'Forms';
+  users = [];
+  onSubmit() {
+    this.users.push(this.user);
+    this.user = { email: '' };
+    console.log(this.users);
+  }
 }
